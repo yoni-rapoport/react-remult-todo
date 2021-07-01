0a891a980b79e9bfae246ef1f925e92ffb31dade
Noam Honig <noam@fireflymigration.com>
2021-07-01 06:42:36 +0300

    added task editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Context } from '@remult/core';
 import React, { useState } from 'react';
 import { Task } from './Task';
+import { set } from '@remult/core/set'
+
 const context = new Context();
 function App() {
   const [title, setTaskTitle] = useState("");
@@ -28,10 +30,25 @@ function App() {
       <input value={title} onChange={(e) => setTaskTitle(e.target.value)} />
       <button onClick={createTask}>Create Task</button>
       <ul>
-        {state.tasks.map(t => (<li>{t.title} <button onClick={() => deleteTask(t)}>Delete</button> </li>))}
+        {state.tasks.map(t => (<li><TaskEditor task={t} /> <button onClick={() => deleteTask(t)}>Delete</button> </li>))}
       </ul>
     </div>
   );
 }
 
 export default App;
+
+type Props = {
+  task: Task
+};
+
+const TaskEditor: React.FC<Props> = ({ task }) => {
+  const [state, setState] = useState({ ...task, changed: false })
+
+  const save = () => set(task, state).save().then(t => setState({ ...task, changed: false }));
+
+  return <span>
+    <input value={state.title} onChange={e => setState(prev => ({ ...prev, title: e.target.value, changed: true }))} />
+    <button onClick={save} disabled={!state.changed}>Save</button>
+  </span>
+}
